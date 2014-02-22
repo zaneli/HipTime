d61900fcf97e5d2950564b9520287151f5bf46df
Akihito Tsukamoto <tsuka05@gmail.com>
2014-02-22 12:35:03 +0900

    Return dummy response

---
+++ app/hiptime/controllers/StatusController.scala
@@ -0,0 +1,20 @@
+package hiptime.controllers
+
+import play.api.mvc.{Action, Controller}
+import play.api.libs.json.Json
+
+
+object StatusController extends Controller {
+
+  def status = Action {
+    val response = Json.obj(
+      "type" -> "users",
+      "value" -> Json.arr(
+        Json.obj("name" -> "hoge", "email" -> "hoge@example.com", "active" -> true),
+        Json.obj("name" -> "fuga", "email" -> "fuga@example.com", "active" -> true),
+        Json.obj("name" -> "piyo", "email" -> "piyo@example.com", "active" -> false)
+      )
+    )
+    Ok(response)
+  }
+}
